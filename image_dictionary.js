992c1831bf926dba71a8aa971e4fda5b8946e58c
Stefan Gavriliuc <stefan.gavriliuc@ucalgary.ca>
2020-04-14 11:29:07 -0600

    added food to image dictionary for some mcds icons

---
+++ image_dictionary.js
@@ -0,0 +1,43 @@
+var dict  = {
+    "MCDONALD'S_HAMBURGER" : "burger.jpeg",
+    "MCDONALD'S_ FLT-O-FSH" : "burger.jpeg",
+    "MCDONALD'S_ CHEESEBURGER" : "burger.jpeg",
+    "MCDONALD'S_QUARTER POUNDER" : "burger.jpeg",
+    "MCDONALD'S_QUARTER POUNDER W/ CHS" : "burger.jpeg",
+    "MCDONALD'S_BIG MAC" : "burger.jpeg",
+    "MCDONALD'S_FRENCH FR" : "fries.jpeg",
+    "MCDONALD'S_CHICK MCNUGGETS" : "nuggets.png",
+    "MCDONALD'S_BARBEQUE SAU" : "sauce.png",
+    "MCDONALD'S_CREAMY RANCH SAU" : "sauce.png",
+    "MCDONALD'S_HOT MUSTARD SAU" : "sauce.png",
+    "MCDONALD'S_SPICY BUFFALO SAU" : "sauce.png",
+    "MCDONALD'S_SWT 'N SOUR SAU" : "sauce.png",
+    "MCDONALD'S_TANGY HONEY MUSTARD SAU" : "sauce.png",
+    "MCDONALD'S_HASH BROWN" : "hasbrown.jpeg",
+    "MCDONALD'S_HOTCAKES (PLAIN)" : "pancakes.jpg",
+    "MCDONALD'S_BKD APPL PIE" : "pancakes.jpg", //TODO: find apple pie?
+    "MCDONALD'S_BACON_EGG & CHS MCGRIDDLES" : "burger.jpeg",
+    "MCDONALD'S_SAUSAGE MCGRIDDLES" : "burger.jpeg",
+    "MCDONALD'S_SAUSAGE_EGG & CHS MCGRIDDLES" : "burger.jpeg",
+    "MCDONALD'S_VANILLA RED FAT ICE CRM CONE" : "icecream.png",
+    "MCDONALD'S_STRAWBERRY SUNDAE" : "sundae.jpeg",
+    "MCDONALD'S_HOT CARAMEL SUNDAE" : "sundae.jpeg",
+    "MCDONALD'S_HOT FUDGE SUNDAE" : "sundae.jpeg",
+    "MCDONALD'S_PNUTS (FOR SUNDAES)" : "sundae.jpeg", //TODO: find peanuts
+    "MCDONALD'S_MCFLURRY W/ M&M'S CANDIES" : "icecream.png", //TODO: mcflurries
+    "MCDONALD'S_MCFLURRY W/ OREO COOKIES" : "icecream.png",
+    "MCDONALD'S_SAUSAGE BURRITO" : "burrito.jpeg",
+    "MCDONALD'S_BIG BRKFST" : "burger.jpge",
+    "MCDONALD'S_LOFAT CARAMEL SAU" : "sauce.png",
+    "MCDONALD'S_DOUBLE CHEESEBURGER" : "burger.jpeg",
+    "MCDONALD'S_DOUBLE QUARTER POUNDER W/ CHS" : "burger.jpeg",
+    "MCDONALD'S_NEWMAN'S OWN COBB DRSNG" : "dressing.png",
+    "MCDONALD'S_NEWMAN'S OWN CREAMY CAESAR DRSNG" : "dressng.png",
+    "MCDONALD'S_NEWMAN'S OWN LOFAT BALSAMIC VINAIGRETTE" : "dressing.png",
+    "MCDONALD'S_NEWMAN'S OWN RANCH DRSNG" : "dressing.png",
+    "MCDONALD'S_BIG MAC (WITHOUT BIG MAC SAUCE)" : "burger.jpeg",
+
+
+
+};
+
