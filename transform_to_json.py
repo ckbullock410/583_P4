67b2db5d56f74c0182fb4511ddc99a0155085314
Chandler <ckbullock410@gmail.com>
2020-04-13 21:24:46 -0700

    some baseline interactions working

---
+++ transform_to_json.py
@@ -0,0 +1,32 @@
+import json
+
+with open("./fast_food.csv", 'r') as d:
+    lines = d.readlines()
+    restaurants = {
+                    "McDonald's": [],
+                    "Dominos": [],
+                    "Wendy's": [],
+                    "Popeyes": [],
+                    "Burger King": [],
+                    "Pizza Hut": [],
+                    "KFC": [],
+                    "Taco Bell": []
+                }
+
+    for line in lines[1:]:
+        _, restaurant, description, calories, protein, fat, iron, sodium, cholesterol, carbohydrates, water = line.split(',')
+        water = water.strip()  # get rid of \n
+        item = {}
+        item["calories"] = calories
+        item["sodium"] = sodium
+        item["iron"] = iron
+        item["cholesterol"] = cholesterol
+        item["name"] = description
+        item["carbs"] = carbohydrates
+        item["protein"] = protein
+        item["fat"] = fat
+        item["water"] = water
+        restaurants[restaurant].append(item)
+
+with open("data.json", "w") as j:
+    json.dump(restaurants, j, indent=4)
